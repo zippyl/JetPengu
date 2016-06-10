5ad3967ab5c4a4df48662c350dfc93a8b33474a5
zippyl <zippy@ex.ua>
2016-06-10 18:59:45 +0300

    ui done, adding scripts to ui

---
+++ Assets/Scripts/UI/Menu/PlayButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+
+    [SerializeField] private Image _playImage;
+    [SerializeField] private Sprite _spriteOnClick;
+    [SerializeField] private Sprite _spriteDefault;
+
+    // Use this for initialization
+    void Start()
+    {
+        _playImage.sprite = _spriteDefault;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void PlayButtonClick(int scene)
+    {
+        StartCoroutine(Play(scene));
+    }
+
+    IEnumerator Play(int scene)
+    {
+        _playImage.sprite = _spriteOnClick;
+        yield return new WaitForSeconds(.3f);
+        SceneManager.LoadScene(scene);
+    }
+}
